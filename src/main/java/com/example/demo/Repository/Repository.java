671888d05636ce55dadbc66483b7627834367bd9
Aryan Pahariya <127323817+aryanpahariya07@users.noreply.github.com>
2024-08-27 09:54:46 +0530

    Delete src directory

--- src/main/java/com/example/demo/Repository/Repository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.Entity.User;
-
-public interface Repository extends CrudRepository<User,String>{
-    
-}
